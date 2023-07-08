4293dba247de5500647ab4061480b41af555d9e6
yuvrajjangir <yuvijangir78@gmail.com>
2023-07-08 11:34:40 +0530

    cartpage

--- cartpage/deliveryAdd.js
+++ cartpage/deliveryAdd.js
@@ -1,69 +1,14 @@
-// Function to save the address
-function saveAddress(event) {
-  event.preventDefault(); // Prevent form submission
+document.getElementById('payment-btn').addEventListener('click', function(event) {
+  event.preventDefault();
 
-  // Get the input values
-  var nameInput = document.getElementById('name');
-  var mobileInput = document.getElementById('Mnum');
-  var addressInput = document.getElementById('addres');
-  var pinInput = document.getElementById('pin');
-  var addressTypeSelect = document.getElementById('atype');
+  var name = document.getElementById('name').value;
+  var mobile = document.getElementById('mobile').value;
+  var address = document.getElementById('address').value;
+  var pincode = document.getElementById('pincode').value;
 
-  // Get the values entered by the user
-  var name = nameInput.value;
-  var mobileNumber = mobileInput.value;
-  var address = addressInput.value;
-  var pinCode = pinInput.value;
-  var addressType = addressTypeSelect.value;
-
-  // Validate the address inputs
-  if (name.trim() === '' || mobileNumber.trim() === '' || address.trim() === '' || pinCode.trim() === '') {
-    alert('Please fill all the address fields.');
-    return;
+  if (name === '' || mobile === '' || address === '' || pincode === '') {
+    alert('Please fill in all the fields before proceeding to payment.');
+  } else {
+    window.location.href = 'payment.html';
   }
-
-  // Perform the logic to save the address
-  // You can add your code here to save the address details in your system
-  console.log('Name:', name);
-  console.log('Mobile Number:', mobileNumber);
-  console.log('Address:', address);
-  console.log('Pin Code:', pinCode);
-  console.log('Address Type:', addressType);
-
-  // Clear the form fields after saving the address
-  nameInput.value = '';
-  mobileInput.value = '';
-  addressInput.value = '';
-  pinInput.value = '';
-  addressTypeSelect.value = 'home'; // Reset the address type to the default value
-}
-
-// Wait for the document to finish loading
-document.addEventListener('DOMContentLoaded', function () {
-  // Get the form element
-  var addressForm = document.getElementById('address');
-
-  // Add event listener to the form submission
-  addressForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get the input values
-    var nameInput = document.getElementById('name');
-    var mobileInput = document.getElementById('Mnum');
-    var addressInput = document.getElementById('addres');
-    var pinInput = document.getElementById('pin');
-
-    // Check if any of the address fields are empty
-    if (
-      nameInput.value.trim() === '' ||
-      mobileInput.value.trim() === '' ||
-      addressInput.value.trim() === '' ||
-      pinInput.value.trim() === ''
-    ) {
-      alert('Please fill all the address fields.');
-    } else {
-      // All fields are filled, redirect to payment.html
-      window.location.href = 'payment.html';
-    }
-  });
 });
